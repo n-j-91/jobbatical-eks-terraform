b394cd9ae7fbaa63b5280f59dfb49264747e824f
Nayanajith Chandradasa <nayanajith@Nayanajiths-MacBook-Pro.local>
2018-06-24 00:43:42 +0530

    Run script added

--- run.sh
+++ run.sh
@@ -7,5 +7,4 @@ terraform output jobbatical-kubeconfig > jobbatical-kubeconfig
 terraform output config-map-aws-auth > config-map-aws-auth.yaml
 
 mkdir -p ./.kube && mv jobbatical-kubeconfig ./.kube/
-export KUBECONFIG=$KUBECONFIG:$(pwd)"/.kube/jobbatical-kubeconfig"
-kubectl apply -f config-map-aws-auth.yaml
+KUBECONFIG=$(pwd)"/.kube/jobbatical-kubeconfig" kubectl apply -f config-map-aws-auth.yaml
